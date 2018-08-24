1103f86980218a31c589463e66c5f03b68fc5c7e
cln19921024 <642132287@qq.com>
2018-08-24 12:59:32 +0800

    init

---
+++ src/main/java/com/zhuangtaiji/zhuangtaiji/ZhuangtaijiApplication.java
@@ -0,0 +1,12 @@
+package com.zhuangtaiji.zhuangtaiji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhuangtaijiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhuangtaijiApplication.class, args);
+	}
+}
